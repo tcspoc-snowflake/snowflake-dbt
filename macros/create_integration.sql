de9b878d1c009e3704c0f5135438385cf4cde526
tcsdevopspoc <tcspoc-sk@outlook.com>
2023-06-05 17:52:28 +0530

    Updated project to include object management

---
+++ macros/create_integration.sql
@@ -0,0 +1,55 @@
+{%- macro create_integration(file) -%}
+
+{% if not file %}
+  {{ exceptions.raise_compiler_error(
+    "\nyou must pass in a file via arguments:" ~
+    "\n  --args 'file: ./snowflake/integration/adls_to_snowflake_integration.yml'"
+  ) }}
+{% endif %}
+
+{% set integration = gather_results(file) %}
+
+{% set integration_name = integration.pop('integration_name') %}
+{% set integration_type = integration.pop('integration_type') %}
+{% set create_attributes %}
+  {%- for key, value in integration.items() %}
+    {%- if value is iterable and value is not string %}
+      {{ key }} = ({{ "\'" + value | join("\', \'") + "\'" }})
+    {%- else %}
+      {{ key }} = {{ value }}
+    {%- endif %}
+  {%- endfor %}
+{% endset %}
+
+{% set alter_attributes %}
+  {%- for key, value in integration.items() if key not in ['type', 'storage_provider'] %}
+    {%- if value is iterable and value is not string %}
+      {{ key }} = ({{ "\'" + value | join("\', \'") + "\'" }})
+    {%- else %}
+      {{ key }} = {{ value }}
+    {%- endif %}
+  {%- endfor %}
+{% endset %}
+
+
+{%- set sql -%}
+begin name create_integration;
+
+{# don't want to delete if already exists :) #}
+create {{ integration_type }} integration if not exists {{ integration_name }}
+{{- create_attributes }}
+;
+
+alter {{ integration_type }} integration {{ integration_name }} set
+{{- alter_attributes }}
+;
+
+commit;
+{%- endset -%}
+
+{{ log(sql, info=True) }}
+{% if not var('dry_run', False) %}
+  {{ run_query(sql) }}
+{% endif %}
+
+{%- endmacro -%}
---
+++ macros/create_users.sql
@@ -0,0 +1,59 @@
+{% macro create_users() %}
+
+{% set file = var('snowflake_user_file' , 'snowflake/users/users.yml') %}
+{%- set _password = var('password', 's0up3rs$cr3t') %}
+{% set must_quote_columns = ['email', 'comment', 'rsa_public_key', 'display_name', 'first_name', 'last_name', 'middle_name'] %}
+
+{% set user_list = gather_results(file) %}
+
+
+{%- set user_sql -%}
+
+-- set single transaction to rollback if errors
+begin name create_users;
+use role {{ var('snowflake_admin', 'securityadmin') }};
+
+{% for user in user_list -%}
+
+  {%- set user_name = user.pop('name') %}
+  {%- set attributes = user.get('attributes') -%}
+  {%- set roles = user.get('roles', ['PUBLIC']) %}
+
+  create user
+    if not exists {{ user_name }}
+    password = '{{ _password }}'
+    must_change_password = true
+  ;
+
+  alter user {{ user_name }} set
+    {% for key, value in attributes.items() -%}
+    {%- if key in must_quote_columns or ' ' in value|string -%}
+    {{ key }} = {{ "'" ~ value ~ "'" }}
+    {%- elif key.split('_')[0] == 'default' -%}
+    {{ key }} = {{ value.upper() }}
+    {%- else -%}
+    {{ key }} = {{ value }}
+    {%- endif %}
+    {% endfor -%}
+  ;
+
+  {% for role in roles -%}
+  grant role
+    {{ role.upper() }} to
+    user {{ user_name }}
+  ;
+  {% endfor -%}
+
+{%- endfor -%}
+
+commit;
+
+{%- endset %}
+
+
+{% do log(user_sql, info=True) %}
+{% if not var('dry_run', False) %}
+  {{ run_query(user_sql) }}
+{% endif %}
+
+{% endmacro %}
